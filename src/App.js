f350490bf74750d9bcf8b9d7c2f1a55fa9829210
sugama <reiji.sugama@gmail.com>
2024-07-17 10:58:40 +0900

    OX切り替えのテスト

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Square({ props }) {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    console.log(`props: ${props}`);
+    setValue(props);
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [value, setValue] = useState("X");
+
+  function handleValue() {
+    console.log(`value: ${value}`);
+    if (value == "X") {
+      setValue("O");
+    } else {
+      setValue("X");
+    }
+  }
+
+  return (
+    <>
+      <Square props={value} onClick={handleValue} />
+      <Square props={value} onClick={handleValue} />
+      <Square props={value} onClick={handleValue} />
+    </>
+  );
 }
